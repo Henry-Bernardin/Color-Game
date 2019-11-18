aa7e4237a3d61803cc5dfe07b865a785d71854c3
Henry-Bernardin <henry.bernardin@gmail.com>
2019-11-18 12:16:39 -0500

    Js to generate num amount of random colors

--- js/index.js
+++ js/index.js
@@ -1,16 +1,10 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square"); 
 var pickedColor = pickedColor(); 
 var colorDisplay = document.getElementById("colorDisplay"); 
-var messageDisplay = document.getElementById("message"); 
+var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");  
 
 
 colorDisplay.textContent = pickedColor;
@@ -26,6 +20,7 @@ for(var i = 0; i < squares.length; i++){
     if (clickedColor === pickedColor){
         messageDisplay.textContent = "Correct!";
         changeColors(pickedColor); 
+        h1.style.backgroundColor = pickedColor; 
     } else {
         this.style.backgroundColor = "#232323"; 
         messageDisplay.textContent = "Try Again!"; 
@@ -44,4 +39,24 @@ function changeColors(color){
 function pickedColor(){
     var random = Math.floor(Math.random() * colors.length); 
     return colors [random]; 
+}
+
+function generateRandomColors(num) {
+    //create an array
+    var array = []; 
+    //repeat num times
+    for (var i = 0; i < num; i++){
+    //add reandom color and push into array
+    array.push(randomColor());  
+    }
+    //return array 
+    return array; 
+}
+
+function randomColor () {
+    var r = Math.floor(Math.random() * 256); 
+    var g = Math.floor(Math.random() * 256); 
+    var b = Math.floor(Math.random() * 256); 
+    //rgb(r, g, b)
+    return "rgb(" + r + ", " + g + ", " + b + ")"; 
 }
