8c53fd05b9cf36ff1d58f31ca4e0cdbc3bb79d09
Henry-Bernardin <henry.bernardin@gmail.com>
2019-11-20 13:24:58 -0500

    logic added for easy button

--- js/index.js
+++ js/index.js
@@ -12,6 +12,16 @@ var hardBtn = document.getElementById("hardBtn");
 easyBtn.addEventListener("click", function(){
     easyBtn.classList.add("selected");
     hardBtn.classList.remove("selected");
+    colors = generateRandomColors(3);
+    pickedColor = pickColor(); 
+    colorDisplay.textContent = pickedColor; 
+    for(var i = 0; i < squares.length; i++){
+        if(colors[i]){
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
 }) 
 
 hardBtn.addEventListener("click", function(){
